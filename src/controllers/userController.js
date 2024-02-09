73594b8dd572f97860d1078def9a14b2b55025cf
Your Name <roihansori12@gmail.com>
2024-02-09 19:19:55 +0700

    feat: create delete user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -63,5 +63,64 @@ const updateUser = async (req, res, next) => {
     );
   }
 };
+const deleteUser = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const userDelete = await User.destroy({
+      where: {
+        userId: userId,
+      },
+    });
+    if (!userDelete) {
+      return res.status(404).json({
+        errors: ["User not found"],
+        message: "Delete user failed",
+        data: null,
+      });
+    } else {
+      return res.status(200).json({
+        errors: [],
+        message: "Delete user successfully",
+        data: null,
+      });
+    }
+  } catch (error) {
+    next(
+      new Error("controllers/authController.js:deleteUser - " + error.message)
+    );
+  }
+};
 
-export { updateUser };
+const getUser = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findOne({
+      where: {
+        userId: userId,
+      },
+    });
+    if (!user) {
+      return res.status(404).json({
+        errors: ["User not found"],
+        message: "Get user failed",
+        data: null,
+      });
+    } else {
+      const userResponse = {
+        userId: user.userId,
+        fullName: user.fullName,
+        email: user.email,
+        createdAt: user.createdAt,
+      };
+      return res.status(200).json({
+        errors: [],
+        message: "Get user successfully",
+        data: userResponse,
+      });
+    }
+  } catch (error) {
+    next(new Error("controllers/authController.js:getUser - " + error.message));
+  }
+};
+
+export { updateUser, deleteUser , getUser};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,16 @@
-import express from 'express';
-import { authentication } from '../controllers/errorHandlingController.js';
-import { updateUser } from '../controllers/userController.js';
+import express from "express";
+import { authentication } from "../controllers/errorHandlingController.js";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+} from "../controllers/userController.js";
 
 const userRoutes = express.Router();
 
-userRoutes.patch('/users/:id',authentication, updateUser )
-export default userRoutes
+userRoutes.patch("/users/:id", authentication, updateUser);
+userRoutes.delete("/users/:id", authentication, deleteUser);
+userRoutes.get("/users/:id", authentication, getUser);
+
+
+export default userRoutes;
